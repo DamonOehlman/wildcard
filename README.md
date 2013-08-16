39d8e09b6bafa0830fb181bbfdf3b3ec394d0077
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-16 20:52:26 +1000

    Generate docs, update travis testing options

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # wildcard
 
-Very simple wildcard matching, which is designed to provide the same functionality that is found in the [eve](https://github.com/DmitryBaranovskiy/eve) eventing library.
+Very simple wildcard matching, which is designed to provide the same
+functionality that is found in the
+[eve](https://github.com/adobe-webplatform/eve) eventing library.
+
+
+[![NPM](https://nodei.co/npm/wildcard.png)](https://nodei.co/npm/wildcard/)
+
+[![Build Status](https://travis-ci.org/DamonOehlman/wildcard.png?branch=master)](https://travis-ci.org/DamonOehlman/wildcard)
+[![stable](http://hughsk.github.io/stability-badges/dist/stable.svg)](http://github.com/hughsk/stability-badges)
 
 [![browser support](https://ci.testling.com/DamonOehlman/wildcard.png)](https://ci.testling.com/DamonOehlman/wildcard)
 
-<a href="http://travis-ci.org/#!/DamonOehlman/wildcard"><img src="https://secure.travis-ci.org/DamonOehlman/wildcard.png" alt="Build Status"></a>
+
+## Usage
 
 It works with strings:
 
@@ -39,6 +48,7 @@ var testdata = {
 wildcard('a.*.c', testdata); // { 'a.b.c': {} }
 ```
 
-While the library works in Node, if you are are looking for file-based wildcard matching then you should have a look at:
+While the library works in Node, if you are are looking for file-based
+wildcard matching then you should have a look at:
 
-https://github.com/isaacs/node-glob
+<https://github.com/isaacs/node-glob>
--- index.js
+++ index.js
@@ -1,62 +1,111 @@
-// umdjs returnExports pattern: https://github.com/umdjs/umd/blob/master/returnExports.js
+/**
+  # wildcard
+
+  Very simple wildcard matching, which is designed to provide the same
+  functionality that is found in the
+  [eve](https://github.com/adobe-webplatform/eve) eventing library.
+
+  ## Usage
+
+  It works with strings:
+
+  ```js
+  wildcard('foo.*', 'foo.bar'); // true
+  wildcard('foo.*', 'foo'); // true
+  ```
+
+  Arrays:
+
+  ```js
+  var testdata = [
+      'a.b.c',
+      'a.b',
+      'a',
+      'a.b.d'
+  ];
+
+  wildcard('a.b.*', testdata); // ['a.b.c', 'a.b', 'a.b.d']
+  ```
+
+  Objects (matching against keys):
+
+  ```js
+  var testdata = {
+      'a.b.c' : {},
+      'a.b'   : {},
+      'a'     : {},
+      'a.b.d' : {}
+  };
+
+  wildcard('a.*.c', testdata); // { 'a.b.c': {} }
+  ```
+
+  While the library works in Node, if you are are looking for file-based
+  wildcard matching then you should have a look at:
+
+  <https://github.com/isaacs/node-glob>
+**/
 (function (root, factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else {
-        root['wildcard'] = factory();
-    }
+  if (typeof exports === 'object') {
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else {
+    root.wildcard = factory();
+  }
 }(this, function () {
-    var reSep = /[\/\.]/;
+  'use strict';
+
+  var reSep = /[\/\.]/;
+  
+  function WildcardMatcher(text) {
+    this.parts = (text || '').split(reSep);
+  }
+  
+  WildcardMatcher.prototype.match = function(input) {
+    var matches = true;
+    var parts = this.parts;
+    var ii;
+    var partsCount = parts.length;
+    var testParts;
     
-    function WildcardMatcher(text) {
-        this.parts = (text || '').split(reSep);
+    if (typeof input == 'string' || input instanceof String) {
+      testParts = (input || '').split(reSep);
+      for (ii = 0; matches && ii < partsCount; ii++) {
+        matches = parts[ii] === '*' || parts[ii] === testParts[ii];
+      }
+    }
+    else if (typeof input.splice == 'function') {
+      matches = [];
+      
+      for (ii = input.length; ii--; ) {
+        if (this.match(input[ii])) {
+          matches[matches.length] = input[ii];
+        }
+      }
+    }
+    else if (typeof input == 'object') {
+      matches = {};
+      
+      for (var key in input) {
+        if (this.match(key)) {
+          matches[key] = input[key];
+        }
+      }
     }
     
-    WildcardMatcher.prototype.match = function(input) {
-        var matches = true,
-            parts = this.parts, 
-            ii, partsCount = parts.length;
-        
-        if (typeof input == 'string' || input instanceof String) {
-            var testParts = (input || '').split(reSep);
+    return matches;
+  };
+  
+  function wildcard(text, test) {
+    var matcher = new WildcardMatcher(text);
     
-            for (ii = 0; matches && ii < partsCount; ii++) {
-                matches = parts[ii] === '*' || parts[ii] === testParts[ii];
-            }
-        }
-        else if (typeof input.splice == 'function') {
-            matches = [];
-            
-            for (ii = input.length; ii--; ) {
-                if (this.match(input[ii])) {
-                    matches[matches.length] = input[ii];
-                }
-            }
-        }
-        else if (typeof input == 'object') {
-            matches = {};
-            
-            for (var key in input) {
-                if (this.match(key)) {
-                    matches[key] = input[key];
-                }
-            }
-        }
-        
-        return matches;
-    };
-    
-    function wildcard(text, test) {
-        var matcher = new WildcardMatcher(text);
-        
-        if (typeof test != 'undefined') {
-            return matcher.match(test);
-        }
-    
-        return matcher;
+    if (typeof test != 'undefined') {
+      return matcher.match(test);
     }
-    
-    return typeof wildcard != 'undefined' ? wildcard : undefined;
+
+    return matcher;
+  }
+  
+  return typeof wildcard != 'undefined' ? wildcard : undefined;
 }));
