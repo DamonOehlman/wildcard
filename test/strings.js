4797dae91b024f00c48dfc3acdbfc9499e3591e7
Damon Oehlman <damon.oehlman@gmail.com>
2019-01-07 17:32:40 +1100

    Properly support wildcard when not in a section of its own (#11)

--- test/strings.js
+++ test/strings.js
@@ -3,13 +3,22 @@ var test = require('tape'),
 
 test('general wild card matching tests', function(t) {
 
-    t.plan(6);
+    t.plan(7);
     t.ok(wildcard('foo.*', 'foo.bar'), 'foo.* should match foo.bar');
     t.ok(wildcard('foo.*', 'foo'), 'foo.* should match foo');
+    t.ok(wildcard('*.foo.com', 'test.foo.com'), 'test.foo.com should match *.foo.com');
     t.notOk(wildcard('foo.*', 'bar'), 'foo.* should not match bar');
     t.ok(wildcard('a.*.c', 'a.b.c'), 'a.*.c should match a.b.c');
     t.notOk(wildcard('a.*.c', 'a.b'), 'a.*.c should not match a.b');
-    t.notOk(wildcard('a', 'a.b.c'), 'a should not match a.b.c');
+    t.notOk(wildcard('a', 'a.b.c'), 'a should not match a.b.c');    
+});
+
+test('regex wildcard matching tests', function(t) {
+  t.plan(4);
+  t.ok(wildcard('*foo', 'foo'), '*foo should match foo');
+  t.ok(wildcard('*foo.b', 'foo.b'), '*foo.b should match foo.b');
+  t.ok(wildcard('a.*foo.c', 'a.barfoo.c'), 'a.barfoo.c should match a.*foo.c');
+  t.ok(wildcard('a.foo*.c', 'a.foobar.c'), 'a.foobar.c should match a.foo*.c');
 });
 
 test('general wild card with separator matching tests', function(t) {
